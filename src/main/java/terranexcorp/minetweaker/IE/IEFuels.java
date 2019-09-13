40d32e3c9735b8780fc58c28d224323517aeb3a8
Superfrogman98 <superfrogman98m@gmail.com>
2019-09-13 00:59:14 -0500

    Modified functions, hopefully fixed removing fuels and undo action.

--- src/main/java/terranexcorp/minetweaker/IE/IEFuels.java
+++ src/main/java/terranexcorp/minetweaker/IE/IEFuels.java
@@ -1,23 +1,15 @@
 package terranexcorp.minetweaker.IE;
 
-import blusunrize.immersiveengineering.api.ApiUtils;
-import blusunrize.immersiveengineering.common.util.Utils;
-import cpw.mods.fml.common.registry.GameData;
+
 import minetweaker.IUndoableAction;
 import minetweaker.MineTweakerAPI;
 import minetweaker.api.liquid.ILiquidStack;
 import minetweaker.api.minecraft.MineTweakerMC;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemStack;
 import net.minecraftforge.fluids.Fluid;
-import net.minecraftforge.fluids.FluidStack;
-import net.minecraftforge.oredict.OreDictionary;
+import net.minecraftforge.fluids.FluidRegistry;
 import stanhebben.zenscript.annotations.ZenClass;
 import stanhebben.zenscript.annotations.ZenMethod;
-
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Map;
 
 import blusunrize.immersiveengineering.api.energy.DieselHandler;
 
@@ -26,42 +18,39 @@ public class IEFuels
 {
     //Ex. Biodiesel/125, Fuel/375, Diesel/175
     @ZenMethod
-    public static void addFuel(ILiquidStack fluidstack, int time)
+    public static void addFuel(ILiquidStack fuel, int time)
     {
-        Fluid fluid = MineTweakerMC.getLiquidStack(fluidstack).getFluid();
-
-        MineTweakerAPI.apply(new addIEFuels(fluid, time));
+        MineTweakerAPI.apply(new addIEFuels(fuel, time));
     }
 
     @ZenMethod
-    public static void removeFuel(ILiquidStack fluidstack)
+    public static void removeFuel(ILiquidStack fuel)
     {
-        Fluid fluid = MineTweakerMC.getLiquidStack(fluidstack).getFluid();
-
-        MineTweakerAPI.apply(new removeIEFuels(fluid));
+        MineTweakerAPI.apply(new removeIEFuels(fuel));
     }
 
     private static class addIEFuels implements IUndoableAction
     {
-        Fluid fluid;
-        int burnTime;
+        private final ILiquidStack fuel;
+        private final int time;
 
-        public addIEFuels(Fluid fuel, int time)
+        public addIEFuels(ILiquidStack fuel, int time)
         {
-            this.fluid = fuel;
-            this.burnTime = time;
+            this.fuel = fuel;
+            this.time = time;
         }
 
         @Override
         public void apply()
         {
-            DieselHandler.registerFuel(fluid, burnTime);
+            Fluid fuelFluid = FluidRegistry.getFluid(fuel.getName());
+            DieselHandler.registerFuel(fuelFluid, time);
         }
 
         @Override
         public String describe()
         {
-            return "Adding fuel '" + fluid.getLocalizedName(new FluidStack(fluid, 1000)) + "' to IE fuels.'";
+            return "Registering Diesel Generator Fuel "+fuel.getDisplayName();
         }
 
         @Override
@@ -75,13 +64,13 @@ public class IEFuels
         {
             HashMap<String, Integer> fuelHandler = DieselHandler.getFuelValues();
 
-            fuelHandler.remove(fluid.getName(), burnTime);
+            fuelHandler.remove(fuel.getName());
         }
 
         @Override
         public String describeUndo()
         {
-            return "Removing fuel '" + fluid.getLocalizedName(new FluidStack(fluid, 1000)) + "' from IE fuels.'";
+            return "Removing Diesel Generator Fuel "+fuel.getDisplayName();
         }
 
         @Override
@@ -93,28 +82,26 @@ public class IEFuels
 
     private static class removeIEFuels implements IUndoableAction
     {
-        Fluid fluid;
-        int burnTime;
-
-        public removeIEFuels(Fluid fuel)
+        private final ILiquidStack fuel;
+        private final int time;
+        public removeIEFuels(ILiquidStack fuel)
         {
-            this.fluid = fuel;
-            burnTime = DieselHandler.getBurnTime(fluid);
+            this.fuel = fuel;
+            this.time = DieselHandler.getBurnTime(FluidRegistry.getFluid(fuel.getName()));
         }
 
         @Override
         public void apply()
         {
             HashMap<String, Integer> fuelHandler = DieselHandler.getFuelValues();
-
-            if(burnTime != 0)
-                fuelHandler.remove(fluid.getName(), burnTime);
+            Fluid fuelFluid = FluidRegistry.getFluid(fuel.getName());
+            fuelHandler.remove(fuelFluid);
         }
 
         @Override
         public String describe()
         {
-            return "Removing fuel '" + fluid.getLocalizedName(new FluidStack(fluid, 1000)) + "' from IE fuels.";
+            return "Removing Diesel Generator Fuel "+fuel.getDisplayName();
         }
 
         @Override
@@ -126,14 +113,14 @@ public class IEFuels
         @Override
         public void undo()
         {
-            if(burnTime != 0)
-                DieselHandler.registerFuel(fluid, burnTime);
+            Fluid fuelFluid = FluidRegistry.getFluid(fuel.getName());
+            DieselHandler.registerFuel(fuelFluid,time);
         }
 
         @Override
         public String describeUndo()
         {
-            return "Adding fuel '" + fluid.getLocalizedName(new FluidStack(fluid, 1000)) + "' to IE fuels.";
+            return "Registering Diesel Generator Fuel " + fuel.getDisplayName();
         }
 
         @Override
@@ -143,41 +130,4 @@ public class IEFuels
         }
     }
 
-    static String[][] formatToTable_ItemIntHashmap(Map<String, Integer> map, String valueType)
-    {
-        Map.Entry<String,Integer>[] sortedMapArray = map.entrySet().toArray(new Map.Entry[0]);
-        ArrayList<String[]> list = new ArrayList();
-        try{
-            for(int i=0; i<sortedMapArray.length; i++)
-            {
-                String item = null;
-                if(ApiUtils.isExistingOreName(sortedMapArray[i].getKey()))
-                {
-                    ItemStack is = OreDictionary.getOres(sortedMapArray[i].getKey()).get(0);
-                    if(is!=null)
-                        item = is.getDisplayName();
-                }
-                else if(sortedMapArray[i].getKey().contains("::"))
-                {
-                    String[] split = sortedMapArray[i].getKey().split("::");
-                    Item it = GameData.getItemRegistry().getObject(split[0]);
-                    int meta = 0;
-                    try{meta = Integer.parseInt(split[1]);}catch(Exception e){}
-                    if(it!=null)
-                        item = new ItemStack(it, 1, meta).getDisplayName();
-                }
-                else
-                    item = sortedMapArray[i].getKey();
-
-                if(item!=null)
-                {
-                    int bt = sortedMapArray[i].getValue();
-                    String am = bt+" "+valueType;
-                    list.add(new String[]{item,am});
-                }
-            }
-        }catch(Exception e)	{}
-        String[][] table = list.toArray(new String[0][]);
-        return table;
-    }
 }
