4966a7a403f172107a63f7d7d7289a082dca735e
Superfrogman98 <frogman98m@aol.com>
2022-05-04 21:34:15 -0500

    Stripped down mod to Rain Event Fix

--- src/main/java/terranexcorp/TerraNexCorp.java
+++ src/main/java/terranexcorp/TerraNexCorp.java
@@ -1,33 +1,12 @@
 package terranexcorp;
 
-import java.awt.image.BufferedImage;
 import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
-import java.nio.ByteBuffer;
-import java.util.Arrays;
 
-import cpw.mods.fml.common.Loader;
-import cpw.mods.fml.relauncher.Side;
-import net.minecraft.client.Minecraft;
-import net.minecraft.util.Util;
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.fluids.Fluid;
-import net.minecraftforge.fluids.FluidRegistry;
-import org.lwjgl.opengl.Display;
+
 import terranexcorp.core.TNCCommonProxy;
 import terranexcorp.core.TNCDetails;
-import terranexcorp.core.TNCItems;
-import terranexcorp.core.TNCBlocks;
-import terranexcorp.core.TNCConfig;
-import terranexcorp.core.compat.TNCOreDict;
-import terranexcorp.eventhandlers.RainOverride;
-import terranexcorp.items.ItemHeat;
-import terranexcorp.minetweaker.TNCminetweaker;
-import terranexcorp.worldgen.WorldGenSampleRocks;
 
 
-import cpw.mods.fml.common.FMLCommonHandler;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.Mod.Instance;
@@ -35,9 +14,6 @@ import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
-import cpw.mods.fml.common.registry.GameRegistry;
-
-import javax.imageio.ImageIO;
 
 @Mod(modid = TNCDetails.ModID, name = TNCDetails.ModName, version = TNCDetails.ModVersion, dependencies = TNCDetails.ModDependencies)
 public class TerraNexCorp
@@ -57,35 +33,18 @@ public class TerraNexCorp
 	public void preInitialize(FMLPreInitializationEvent event)
 	{
 		instance = this;
-		TNCConfig.init(event.getSuggestedConfigurationFile());
 		proxy.preinit();
-		FMLCommonHandler.instance().bus().register(new TNCConfig());
-		TNCItems.init();
-		TNCBlocks.init();
-		ItemHeat.setupItemHeat();
-		GameRegistry.registerWorldGenerator(new WorldGenSampleRocks(), 5);
-		Fluid fluidWater = FluidRegistry.getFluid("water");
 	}
 
 	@EventHandler
 	public void initialize(FMLInitializationEvent event)
 	{
 		proxy.init();
-		proxy.hideNEIItems();
-		TNCOreDict.addOreDict();
-		if (Loader.isModLoaded(TNCDetails.MODID_TFCTECH)) {
-			TNCOreDict.tfcTechOre();
-		}
-		if (Loader.isModLoaded(TNCDetails.MODID_CUSTOM)){
-			TNCOreDict.customItemsFrogs();
-		}
 	}
 
 	@EventHandler
 	public void postInitialize(FMLPostInitializationEvent event)
 	{
-		TNCminetweaker.postInit();
-		TNCConfig.reloadOres();
 	}
 
 
--- src/main/java/terranexcorp/core/TNCDetails.java
+++ src/main/java/terranexcorp/core/TNCDetails.java
@@ -2,25 +2,18 @@ package terranexcorp.core;
 
 public class TNCDetails
 {
-	public static final String ModID = "tnc";
-	public static final String ModName = "Terra-Nex-Corp";
-    public static final String ModVersion = "0.0.19";
+	public static final String ModID = "TFCERF";
+	public static final String ModName = "Terrafirmacraft-EventRainFix";
+    public static final String ModVersion = "1";
 
-	public static final String ModChannel = "Terra-Nex-Corp";
 	public static final String SERVER_PROXY_CLASS = "terranexcorp.core.TNCCommonProxy";
 	public static final String CLIENT_PROXY_CLASS = "terranexcorp.core.TNCClientProxy";
 
 	public static final String AssetPath = "/assets/" + ModID + "/";
 
-	public static final String MODID_TFC = "terrafirmacraft";
-	public static final String MODNAME_TFC = "TerraFirmaCraft";
-	public static final String MODID_NEI = "NotEnoughItems";
-	public static final String MODID_TFCTECH = "tfctech";
-	public static final String MODID_CUSTOM = "customitems";
 
 	public static final String ModDependencies =
 			"required-after:Forge@[10.13.4.1558,);"
-					+ "required-after:terrafirmacraft@[0.79.26,);"
-					+ "after:tfctech";
+					+ "required-after:terrafirmacraft@[0.79.26,);";
 
 }
